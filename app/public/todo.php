0ed0e4334319fba51f495f37cd40011a5674328e
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-01-23 23:52:52 +0100

    deleting update form. adding alert message

--- app/public/todo.php
+++ app/public/todo.php
@@ -1,30 +1,36 @@
 <?php require 'includes/views/header.php'; ?>
 <?php require 'includes/views/navbar.php'; ?>
+<?php include 'classes/todos.contr.classes.php';?>
+
+<?php
+        if (isset($_SESSION['message'])):?>
+        <div class="alert <?php echo $_SESSION['msg_type']?>">
+            <?php
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);   
+            ?>
+        </div>
+        <?php endif;?>
 
 <main>
     <section class="add-section">
         <h1>TO DO LIST!</h1>
         <form action="includes/todos.inc.php" method="post">
-            <input
-            id="taskinput"
-            class="add-section-input"
-            type="text" 
-            name="task" 
-            placeholder="Enter a task"
-            autocomplete="off"
-            required>
-            <input type="hidden" name="id">
-            <button class="btn-add" name="submit_create" type="submit"><ion-icon name="add-outline"></ion-icon></button>
+        <input
+        id="taskinput"
+        class="add-section-input"
+        type="text" 
+        name="task" 
+        placeholder="Enter a task"
+        autocomplete="off"
+        required>
+        <button class="btn-add" name="submit_create" type="submit"><ion-icon name="add-outline"></ion-icon></button>
         </form>
     </section>
     
     
     <section class="show-section">
-        <?php
-        require_once 'classes/todos.contr.classes.php';
-        $task = new TodosContr();
-        $rows = $task->setTodos();
-        ?>
+        <?php $rows = $task->setTodos();?>
 
         <?php if(!empty($rows->rowCount())) : ?>
         <ul>
@@ -36,7 +42,7 @@
                     <a class="btn-toggle" href="includes/todos.inc.php?as=completed&id=<?php echo $row['todosId']?>"><ion-icon name="checkmark-outline"></ion-icon></a>
                     <?php endif;?>
                     <h2 class="task<?php echo $row['completed'] ? ' completed' : '' ?>"><?php echo $row['task'];?></h2>
-                    <a class="btn-edit" href="index.php?edit=<?php echo $row['todosId']?>"><ion-icon name="create-outline"></ion-icon></a>
+                    <a class="btn-edit" href="todoupdate.php?edit=<?php echo $row['todosId']?>"><ion-icon name="create-outline"></ion-icon></a>
                     <a class="btn-del" href="includes/todos.inc.php?delete=<?php echo $row['todosId']?>" onclick="return checkDelete()"><ion-icon name="close-outline"></ion-icon></a> 
                     <p class="created">Created by <?php echo $row['users_username']?> on <?php echo $row['created']?></p>
                 </li>
