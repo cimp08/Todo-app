9a22381c102e8fb6159c4c1265e66a720b8d152d
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-01-23 23:53:51 +0100

    cleaning up code. adding edit and update

--- app/public/includes/todos.inc.php
+++ app/public/includes/todos.inc.php
@@ -5,40 +5,21 @@ session_start();
 // Instantiate TodosContr class
 include '../classes/todos.contr.classes.php';
 
-//If the user is not logged in or logged in. 
-if(!isset($_SESSION['user_id'])) {
-    // Head back to login page
-    header('location: ../login.php?error=notloggedin');
-} else {
-    // Head back to todos page
-    header('location: ../todo.php');
-}
-
-    
 
 // If user submitted to create a todo
 if(isset($_POST['submit_create'])) {
      
-    $todosContr = new TodosContr();
-
     // Running add method in todos controller
-    $todosContr->add();
+    $task->add();
 
-    // Head to todo page
-    header('location: ../todo.php?todo=added');
 }
 
 // If user submitted to delete a todo
 if(isset($_GET['delete'])) {
     $id = $_GET['delete'];
 
-    $todosContr = new TodosContr();
-
     // Running delete method in todos controller
-    $todosContr->delete($id);
-
-    // Head to todo page
-    header('location: ../todo.php?todo=deleted');
+    $task->delete($id);
 }
 
 // If user submitted to mark a todo
@@ -46,12 +27,35 @@ if(isset($_GET['as'], $_GET['id'])) {
     $as = $_GET['as'];
     $id = $_GET['id'];
 
-    $todosContr = new TodosContr();
-
-    // Running delete method in todos controller
-    $todosContr->mark($as, $id);
-
-    // Head to todo page
-    header('location: ../todo.php?');
+    // Running mark method in todos controller
+    $task->mark($as, $id);
 
 }
+
+// If user submitted to edit a todo
+if(isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    
+    // Running edit method in todos controller
+    $task->edit($id);
+}
+
+// If user submitted to update a todo
+if(isset($_POST['update'])) {
+    $id = $_POST["id"];
+    $newTask = $_POST['task'];
+    $my_date_time = date("Y-m-d H:i:s", strtotime("+1 hours"));
+    
+    // Running update method in todos controller
+    $task->update($id, $newTask, $my_date_time);
+    
+}
+
+//If the user is not logged in or logged in. 
+if(!isset($_SESSION['user_id'])) {
+    // Head back to login page
+    header('location: ../login.php?error=notloggedin');
+} else {
+    // Head back to todos page
+    header('location: ../todo.php');
+}  
