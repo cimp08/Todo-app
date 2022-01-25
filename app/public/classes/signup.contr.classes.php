5a708a7b4a76263aaff96d11c5076209cfb86a87
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-01-25 11:00:17 +0100

    adding functions for min username length and password length

--- app/public/classes/signup.contr.classes.php
+++ app/public/classes/signup.contr.classes.php
@@ -1,47 +1,63 @@
 <?php
 
-class SignupContr extends Signup {
+class SignupContr extends Signup
+{
     private $username;
     private $password;
     private $repeatPassword;
     private $email;
 
-    public function __construct($username, $password, $repeatPassword, $email){
+    public function __construct($username, $password, $repeatPassword, $email)
+    {
         $this->username = $username;
         $this->password = $password;
         $this->repeatPassword = $repeatPassword;
         $this->email = $email;
     }
 
-    public function signupUser(){
-        if($this->emptyInput() == false){
+    public function signupUser()
+    {
+        if ($this->emptyInput() == false) {
             header('location:../signup.php?error=emptyinput');
             exit();
         }
-        if($this->invalidUsername() == false){
+        if ($this->invalidUsername() == false) {
             header('location:../signup.php?error=username');
             exit();
         }
-        if($this->invalidEmail() == false){
+
+        if ($this->invalidLengthUsername() == false) {
+            header('location:../signup.php?error=usernamelength');
+            exit();
+        }
+
+        if ($this->invalidEmail() == false) {
             header('location:../signup.php?error=email');
             exit();
         }
-        if($this->passwordMatch() == false){
+
+        if ($this->passwordLength() == false) {
+            header('location:../signup.php?error=passwordlength');
+            exit();
+        }
+
+        if ($this->passwordMatch() == false) {
             header('location:../signup.php?error=passwordmatch');
             exit();
         }
-        if($this->userTakenCheck() == false){
+        if ($this->userTakenCheck() == false) {
             header('location:../signup.php?error=useroremailtaken');
             exit();
         }
 
-        $this->setUser($this->username, $this->password, $this->email );
+        $this->setUser($this->username, $this->password, $this->email);
     }
 
     // Checks if any fields are empty
-    private function emptyInput(){
+    private function emptyInput()
+    {
         $result = '';
-        if(empty($this->username || $this->password || $this->repeatPassword || $this->email)){
+        if (empty($this->username || $this->password || $this->repeatPassword || $this->email)) {
             $result = false;
         } else {
             $result = true;
@@ -50,7 +66,8 @@ class SignupContr extends Signup {
     }
 
     // Checks username input
-    function invalidUsername() {
+    function invalidUsername()
+    {
         $result = '';
         if (!preg_match('/^[a-z\d_]{2,20}$/i', $this->username)) {
             $result = false;
@@ -59,11 +76,38 @@ class SignupContr extends Signup {
         }
         return $result;
     }
-    
-    //Checks email input
-    function invalidEmail() {
+
+    // Checks username length input
+    function invalidLengthUsername()
+    {
         $result = '';
-        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+        $username = $this->username;
+        if (strlen($username) < 4) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    //Checks email input
+    function invalidEmail()
+    {
+        $result = '';
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    // Check passwords length
+    function passwordLength()
+    {
+        $result = '';
+        $password = $this->password;
+        if (strlen($password) < 6) {
             $result = false;
         } else {
             $result = true;
@@ -72,9 +116,10 @@ class SignupContr extends Signup {
     }
 
     // Check if password match with repeat
-    function passwordMatch() {
+    function passwordMatch()
+    {
         $result = '';
-        if ($this->password !== $this->repeatPassword){
+        if ($this->password !== $this->repeatPassword) {
             $result = false;
         } else {
             $result = true;
@@ -83,9 +128,10 @@ class SignupContr extends Signup {
     }
 
     // Check if the username or email is already taken 
-    function userTakenCheck() {
+    function userTakenCheck()
+    {
         $result = '';
-        if (!$this->checkUser($this->username, $this->email)){
+        if (!$this->checkUser($this->username, $this->email)) {
             $result = false;
         } else {
             $result = true;
