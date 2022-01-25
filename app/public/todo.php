03b8b603134ae1942688e30fa1bfbed23a6d38a0
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-01-25 13:13:03 +0100

    adding js alert when deleting a task

--- app/public/todo.php
+++ app/public/todo.php
@@ -54,9 +54,9 @@
                   <a class="btn btn-outline-primary float-end" href="todoupdate.php?edit=<?php echo $row['todosId'] ?>">
                     <ion-icon name="create-outline"></ion-icon>
                   </a>
-                  <button type="button" class="btn btn-outline-danger float-end me-1" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                  <a href="includes/todos.inc.php?delete=<?php echo $row['todosId'] ?>" class="btn btn-outline-danger float-end me-1" onclick="return checkDelete()">
                     <ion-icon name="close-outline"></ion-icon>
-                  </button>
+                  </a>
                   <p class="created">Created by <?php echo $row['users_username'] ?> on <?php echo $row['created'] ?></p>
                 </li>
               </ul>
@@ -77,24 +77,5 @@
     <?php endif; ?>
   </section>
 </main>
-
-<!-- Delete Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="deleteModalLabel">Deleting</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Do you want to delete the task?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <a href="includes/todos.inc.php?delete=<?php echo $row['todosId'] ?>" type="button" class="btn btn-danger">Delete</a>
-      </div>
-    </div>
-  </div>
-</div>
 
 <?php require 'includes/views/footer.php'; ?>
