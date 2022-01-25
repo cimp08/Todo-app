c4049830afa4f090a3345738eadc4a9dff160910
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-01-25 21:01:53 +0100

    correcting miss spelled sentence

--- app/public/index.php
+++ app/public/index.php
@@ -5,7 +5,7 @@
     <div class="p-5 mb-4 mt-4 bg-light rounded-3">
       <div class="container-fluid py-5">
         <h1 class="display-6 text-uppercase">Welcome <?php if(isset($_SESSION['user_username'])){echo $_SESSION['user_username'];}?>!</h1>
-        <p class="col-md-8 fs-4">This is a application for you to register personal tasks</p>
+        <p class="col-md-8 fs-4">This is an application for you to register personal tasks</p>
       </div>
     </div>
   </div>
